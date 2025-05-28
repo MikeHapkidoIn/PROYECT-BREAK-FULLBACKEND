8709eead30ba044e9a6aa5c62efedf3b4580d69b
Flor <mariaflorenciaperezd@gmail.com>
2025-05-28 19:22:15 +0200

    bbdd conectada y funcionando

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const dbConnection = async () => {
+try{
+await mongoose.connect(process.env.MONGO_URI)
+console.log('Conexion establecida con BBDD')
+} catch (err) {
+console.error('Error al conectar con MongoDB:', err.message);
+}
+}
+module.exports = dbConnection
