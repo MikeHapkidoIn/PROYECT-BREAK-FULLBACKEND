6e7773ab1ef731e5c2fd0887d4adf65da4a0d393
Flor <mariaflorenciaperezd@gmail.com>
2025-05-29 17:11:35 +0200

    rutas listas

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,25 +2,24 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
+//RUTAS DE VISTA AL PUBLICO
 router.get('/products', productController.showProducts); // Devuelve todos los productos. Cada producto tendrá un enlace a su página de detalle.
 router.get('/products/:productId', productController.showProductById); //Devuelve el detalle de un producto.
-// RUTAS DEL DASHBOARD
-/*
-GET /dashboard: Devuelve el dashboard del administrador. En el dashboard aparecerán todos los artículos que se hayan subido. Si clickamos en uno de ellos nos llevará a su página para poder actualizarlo o eliminarlo.
 
-GET /dashboard/new: Devuelve el formulario para subir un artículo nuevo.
+//RUTAS PARA EL ADMINISTRADOR
+//vista
+router.get('/dashboard', productController.showDashboard); //Devuelve el dashboard del administrador, En el dashboard aparecerán todos los artículos que se hayan subido. Si clickamos en uno de ellos nos llevará a su página para poder actualizarlo o eliminarlo.
+router.get('/dashboard/new', productController.showNewProductForm); // Devuelve el formulario para subir un artículo nuevo.
+router.get('/dashboard/:productId', productController.showProductById); // Devuelve el detalle de un producto en el dashboard.      
+router.get('/dashboard/:productId/edit', productController.showEditProductForm); // Devuelve el formulario para editar un producto.
 
-POST /dashboard: Crea un nuevo producto.
+//crea y actualiza
+router.post('/dashboard', productController.createProduct);//Crea un nuevo producto.
+router.put('/dashboard/:productId', productController.updateProduct); // Actualiza un producto.
 
-GET /dashboard/:productId: Devuelve el detalle de un producto en el dashboard.
-
-GET /dashboard/:productId/edit: Devuelve el formulario para editar un producto.
-
-PUT /dashboard/:productId: Actualiza un producto.
-
-DELETE /dashboard/:productId/delete: Elimina un producto. */
-
+//elimina
+router.delete('/dashboard/:productId/delete', productController.deleteProduct); // Elimina un producto.
+router.post('/dashboard/:productId/delete', productController.deleteProduct); // Elimina un producto. Usamos POST para evitar problemas con los navegadores que no soportan DELETE en formularios.
 
 module.exports = router;
-
 
