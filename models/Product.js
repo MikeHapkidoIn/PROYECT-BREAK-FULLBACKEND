6c8577d24170849e4fcb9f202ec3c81792e2c89e
Flor <mariaflorenciaperezd@gmail.com>
2025-05-29 10:59:14 +0200

    mejora del module export

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 const size = ['XS', 'S', 'M', 'L', 'XL'];  // enum de talles
+const categories = ["Camisetas", "Pantalones", "Zapatos", "Accesorios"]
 const ProductSchema = new mongoose.Schema({
   name:  { type: String, required: true },
   description: { type: String, required: true },
   image: { type: String, required: true },
-  category: { type: String, required: true },
+  category: { type: String, enum: categories, required: true },
   size: { type: String, enum: size, required: true },
   price: { type: Number, required: true, min: 0 }
 }, {timestamps: true });
 
-module.exports = mongoose.model('Product', ProductSchema);
-module.exports.size = size;   // exportado solo si lo necesitas en otros archivos
-
+module.exports = {
+  Product: mongoose.model('Product', ProductSchema),
+  sizes,
+  categories
+};
