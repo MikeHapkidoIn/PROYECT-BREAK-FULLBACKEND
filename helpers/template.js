a3cc7ca7f0c95ce78b898eba2d55ac22b01c2196
Flor <mariaflorenciaperezd@gmail.com>
2025-05-30 10:52:36 +0200

    first commit

---
+++ helpers/template.js
@@ -0,0 +1,10 @@
+//importar los helpers de navbar y basehtml
+//exportarmos funcion que crear la union de todo
+
+const baseHtml = require ('./baseHtml')
+const getNavBar = require ('./getNavBar')
+
+module.exports = function template (content) {
+  const paginaCompleta = getNavBar () + content;
+  return baseHtml (paginaCompleta)
+};
