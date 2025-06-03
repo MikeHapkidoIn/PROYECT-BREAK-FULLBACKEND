b486529d0291f1777433654b6f4a0e1769b1c257
Flor <mariaflorenciaperezd@gmail.com>
2025-06-03 11:40:23 +0200

    todo funcionando. guardando y a refactorizar helpers

--- helpers/baseHtml.js
+++ helpers/baseHtml.js
@@ -1,11 +1,20 @@
-function baseHtml() {
+function baseHtml({ title, navBar, content }) {
   return `
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Mi Tienda Online</title>
-      <link rel="stylesheet" href="/styles.css" />
-    </head>
+    <!DOCTYPE html>
+    <html lang="es">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>${title}</title>
+        <link rel="stylesheet" href="/css/style.css" />
+      </head>
+      <body>
+        ${navBar}
+        <main>
+          ${content}
+        </main>
+      </body>
+    </html>
   `;
 }
 
--- helpers/getNavBar.js
+++ helpers/getNavBar.js
@@ -1,49 +1,25 @@
-/* 
-function getNavbar(isAdmin = false) {
-  const categories = require ('../models/Produtc.js);
+const { categories } = require('../models/Product');
 
-  let navbar = `
-    <nav style="margin-bottom: 2rem;">
-      <a href="/getproducts">Home</a> | 
-  `;
+function getNavBar() {
+  const categoriasHtml = categories.map(cat => `
+    <li><a href="/products?categories=${encodeURIComponent(cat)}">${cat}</a></li>
+  `).join('');
 
-  categories.forEach(cat => {
-    navbar += `<a href="/getproducts?category=${encodeURIComponent(cat)}">${cat}</a> | `;
-  });
-
-  // Login siempre visible
-  navbar += `<a href="/login">Login</a>`;
-
-  // "Nuevo producto" solo si es admin
-  if (isAdmin) {
-    navbar += ` | <a href="/dashboard/products/create">Nuevo producto</a>`;
-  }
-
-  navbar += `</nav>`;
-
-  return navbar;
-}
-
-module.exports = getNavBar;*/
-module.exports = function getNavBar() {
   return `
     <nav>
-      <a href="/">Inicio</a>
-      <a href="/camisetas">Camisetas</a>
-      <a href="/pantalones">Pantalones</a>
-      <a href="/zapatillas">Zapatillas</a>
-      <a href="/accessorios">Accesorios</a>
-      <a href="/dashboard">Dashboard</a>
-      <a href="/">Login</a>
-    </nav>     
-  `;
-};
-/*import { categories } from './enums.js';
-
-const generateNavbar = () => {
-  return `
-    <nav>
-      ${categories.map(cat => `<a href="/productos/categoria/${cat}">${cat}</a>`).join(" | ")}
+      <ul>
+        <li><a href="/">Inicio</a></li>
+        <li>
+          <a href="/products">Productos</a>
+          <ul class="submenu">
+            ${categoriasHtml}
+            <li><a href="/products">Todas</a></li>
+          </ul>
+        </li>
+        <li><a href="/dashboard">Admin</a></li>
+      </ul>
     </nav>
   `;
-}; */
+}
+
+module.exports = getNavBar;
