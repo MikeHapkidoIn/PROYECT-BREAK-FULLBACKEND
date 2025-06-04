57c729767ff3c4e310d5de99e553ef38c0ab6bd3
Flor <mariaflorenciaperezd@gmail.com>
2025-06-04 12:45:10 +0200

    ruta api para json y swagger

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ require('dotenv').config();
 const dbConnecction = require('./config/db.js')
 const productRoutes = require('./routes/productRoutes');
 const methodOverride = require('method-override');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
+const productApiRoutes = require('./routes/productApiRoutes');
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
 app.use(methodOverride(function (req, res) {
@@ -17,7 +21,7 @@ app.use(methodOverride(function (req, res) {
 app.use(express.json())
 
 app.use('/', productRoutes);
-
+app.use('/api', productApiRoutes);
 const PORT = process.env.PORT;
 dbConnecction()
  
